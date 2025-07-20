53cbc1ffe38c2d168300d5678e25cfcdf08205fa
bwright <benwboston@gmail.com>
2025-07-20 04:23:36 +0000

    Fix biome-formatted svelte file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-import { Axis, Cartesian, Figure, LineSeries } from "$lib";
+  import { Axis, Cartesian, Figure, LineSeries } from "$lib";
+  import ScatterSeries from "$lib/scatter-series/scatter-series.svelte";
 
-const randomData = (len = 10) =>
-	Array.from(Array(len)).map(
-		(_, i, arr) =>
-			[i * (1 / arr.length), Math.random() * 3] as [number, number],
-	);
+  const randomData = (len = 10) =>
+    Array.from(Array(len)).map(
+      (_, i, arr) =>
+        [i * (1 / arr.length), Math.random() * 3] as [number, number]
+    );
 
-let series1 = $state(randomData(20));
-setInterval(() => {
-	series1 = randomData(20);
-}, 1000);
+  let series1 = $state(randomData(20));
+  setInterval(() => {
+    series1 = randomData(20);
+  }, 1000);
 
-let series2 = $state(randomData(20));
-setInterval(() => {
-	series2 = randomData(20);
-}, 1000);
+  let series2 = $state(randomData(20));
+  setInterval(() => {
+    series2 = randomData(20);
+  }, 1000);
 </script>
 
 <div class="flex justify-center p-20">
